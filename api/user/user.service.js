703ecc9f81edebc51ec22327f7d23c67ac6b8163
Yaniv bar <yanivb527@gmail.com>
2021-07-22 10:49:08 +0300

    thursday commit

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -104,6 +104,7 @@ async function add(user) {
         }
         const collection = await dbService.getCollection('user')
         await collection.insertOne(userToAdd)
+        console.log(userToAdd)
         return userToAdd
     } catch (err) {
         logger.error('cannot insert user', err)
